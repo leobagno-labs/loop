651d649043a08443261f5f1056fbe01eaab1cd49
Leonardo Bagno <bagnoleonardo@gmail.com>
2022-09-04 04:45:14 -0300

    Adding a Loop solution

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", n, i, n * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
